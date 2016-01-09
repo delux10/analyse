6f848ad4cbbae2b2ff714c096559cbcaeecf739e
hujiong <delux10@qq.com>
2016-01-09 20:39:51 +0800

    创建了数据存放文件夹。

--- fenxi.R
+++ fenxi.R
@@ -3,7 +3,6 @@
 #已经提交到github。
 #加载需要的包
 library(ggplot2)
-setwd("~/Downloads/R数据分析/") 
-read.csv(file = "12m-chusishuju.csv")
-
+setwd("~/Documents/R/data/") 
+mydata <- read.csv("ye_si_chou_jian.csv")
 
