5d3f5ce05bd1e9d85a795b9f8a9abe43e4877ae5
hujiong <delux10@qq.com>
2016-01-09 21:25:51 +0800

    画图
    用ggplot2 画图的探索。

--- fenxi.R
+++ fenxi.R
@@ -3,6 +3,14 @@
 #已经提交到github。
 #加载需要的包
 library(ggplot2)
-setwd("~/Documents/R/data/") 
-mydata <- read.csv("ye_si_chou_jian.csv")
 
+#更改目录，读入文件。
+setwd("~/Documents/R/data/") 
+ye_si_data <- read.csv("ye_si_chou_jian.csv")
+attach(ye_si_data)
+
+#设置主题
+
+#画图
+ys_zsl_pic <- ggplot(ye_si_data)
+ys_zsl_pic + geom_boxplot(aes(x = 牌号, y = 整丝率实测值...),fill = 牌号)
