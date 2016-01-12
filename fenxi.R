e8a6e32ed7d918a4ce01f719cf19c050daad65bc
delux10 <delux10@qq.com>
2016-01-12 12:48:17 +0800

    设置encoding:UTF-8

--- fenxi.R
+++ fenxi.R
@@ -1,17 +1,21 @@
 
 
-#已经提交到github。
-#加载需要的包
+
+#载入库
 library(ggplot2)
 
-#更改目录，读入文件。
-setwd("~/Documents/R/data/") 
-ye_si_data <- read.csv("ye_si_chou_jian.csv")
+
+#读取数据
+setwd("E:/R/data/") 
+ye_si_data <- read.csv("12月贮丝质量.csv")
 attach(ye_si_data)
 
-#设置主题
-
 #画图
+ye_si_pic <- ggplot(ye_si_data)
+ye_si_pic + geom_boxplot(aes(x = 牌号, y = 整丝率实测值...,fill = 牌号))
 
-ys_zsl_pic <- ggplot(data = ye_si_data)
-ys_zsl_pic + geom_boxplot(aes(x = 牌号,y = 整丝率实测值...,fill = 牌号) + ggtitle("叶丝整丝率箱线图")
+
+
+pdf("整丝率箱线图.pdf")
+ye_si_pic
+dev.off()
